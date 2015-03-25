528fb8c7e5b64d175dc3ec4d2b57ac9c09e389c1
David <david.vaynshteyn@gmail.com>
2015-03-25 18:21:47 -0500

    made model and migration for rsvps table

--- db/migrate/20150325225016_create_rsvps.rb
+++ db/migrate/20150325225016_create_rsvps.rb
@@ -1,6 +1,9 @@
 class CreateRsvps < ActiveRecord::Migration
   def change
     create_table :rsvps do |t|
+      t.belongs_to  :game
+      t.belongs_to  :membership
+      t.string      :attending
 
       t.timestamps null: false
     end
