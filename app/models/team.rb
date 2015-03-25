f04bcf167ef0e712f64d13db6a932a8373b1f32e
David <david.vaynshteyn@gmail.com>
2015-03-25 18:04:04 -0500

    made team model and migration

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,6 @@
 class Team < ActiveRecord::Base
+  belongs_to :captain, class_name: "User"
+  has_many   :games
+  has_many   :memberships
+  has_many   :players, through: :memberships
 end
--- db/migrate/20150325224827_create_teams.rb
+++ db/migrate/20150325224827_create_teams.rb
@@ -1,6 +1,9 @@
 class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
+      t.string      :name
+      t.string      :activity
+      t.belongs_to  :captain
 
       t.timestamps null: false
     end
