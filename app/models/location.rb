335198120f0fe342ce22b19b1dc2cb183208b856
David <david.vaynshteyn@gmail.com>
2015-03-25 18:13:21 -0500

    made model and migration for location table

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ActiveRecord::Base
+  has_many  :games
+  has_many  :teams, foreign_key: :home_location_id
 end
--- db/migrate/20150325231001_create_locations.rb
+++ db/migrate/20150325231001_create_locations.rb
@@ -1,6 +1,12 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
+      t.string   :name
+      t.string   :address
+      t.string   :city
+      t.string   :state
+      t.integer  :zip_code
+      t.text     :directions
 
       t.timestamps null: false
     end
