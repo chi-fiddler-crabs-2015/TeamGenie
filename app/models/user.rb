ca7fac69b168cbe8fc69c02096fa817a23657edc
Ricardo Fleury <rfleury2@gmail.com>
2015-03-25 20:00:36 -0500

    Add user model validations and spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
   has_many  :memberships, foreign_key: :player_id
   has_many  :rsvps, through: :memberships
   has_many  :games, through: :rsvps
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, {
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ }
+  }
+  validates :username, {
+    presence: true,
+    uniqueness: true
+  }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many :teams }
+    it { should have_many :memberships }
+    it { should have_many(:rsvps).through(:memberships) }
+    it { should have_many(:games).through(:rsvps) }
+  end
 
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:username) }
+
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_uniqueness_of(:email) }
+
+    it { should allow_value('hello@hello.com').for(:email) }
+    it { should_not allow_value('hellohello.com').for(:email) }
+  end
 end
