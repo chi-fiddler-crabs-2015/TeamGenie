0a5e9ba186e9205d907e4f15b9fcf1b641379a7a
David Vaynshteyn <david.vaynshteyn@gmail.com>
2015-03-26 14:45:10 -0500

    made games controller, need to test

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,19 +8,40 @@ class GameController < ApplicationController
       @game = Game.new
     else
       flash[:notice] = "You must be logged in to create a game"
-      redirect_to login
+      redirect_to login_path
     end
   end
 
   def create
-    @location = Location.find_by_id(params[:location])
-    @game = current_user.games.new(game_params)
-    @game.update_location
-    if @game.save
+    # location = Location.find_by_id(params[:location])
+    game = current_user.games.new(game_params)
+    # update_location(game)
+    if game.save
       redirect_to games_path
     else
       flash[:notice] = "Game was not valid."
     end
+  end
+
+  def destroy
+    if current_user
+      @game = current_game(params[:id])
+      @game.destroy
+    end
+    redirect_to games_path
+  end
+
+  def edit
+    redirect_to edit_game_path(params[:id])
+  end
+
+  def show
+    redirect_to game_path(params[:id])
+  end
+
+  def update
+    @game = current_game(params[:id])
+    @game.update_attributes(params[:game])
   end
 
 end
--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,6 +1,10 @@
 module GameHelper
-  def update_location
-    @game.update_attributes(location: @location)
+  def update_location(game)
+    game.update_attributes(location: @location)
+  end
+
+  def current_game(game_id)
+    Game.find_by(id: game_id)
   end
 end
 
