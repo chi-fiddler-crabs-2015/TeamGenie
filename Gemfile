80887943a480d9c73d97551550c621bfc31b9f7f
David Vaynshteyn <david.vaynshteyn@gmail.com>
2015-03-26 16:37:29 -0500

    User authentication works

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'rails_12factor'
 gem 'faker'
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ require 'faker'
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 team_names = ['Portland Timbers','Los Angeles Galaxy','Seattle Sounders','Houston Dynamo','Real Salt Lake','San Jose Earthquakes','Sporting KC']
-captain = User.create!(first_name: 'Carlos', last_name: 'Dunga', email: 'dunga@dunga.com', username: 'dunga', phone_number: Faker::PhoneNumber.cell_phone, birthday: Faker::Date.between(40.years.ago, 30.years.ago))
+captain = User.create!(first_name: 'Carlos', password: 'hellohello', last_name: 'Dunga', email: 'dunga@dunga.com', username: 'dunga', phone_number: Faker::PhoneNumber.cell_phone, birthday: Faker::Date.between(40.years.ago, 30.years.ago))
 
 10.times do
   Location.create!(name: Faker::Address.street_name, address: Faker::Address.street_address, city: Faker::Address.city, state: Faker::Address.state_abbr, zip_code: Faker::Address.zip, directions: Faker::Lorem.paragraph)
@@ -17,7 +17,7 @@ end
 captain.teams.create!(name: 'Good Times FC', activity: 'Soccer', home_location: Location.all.sample)
 
 50.times do
-  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.safe_email, username: Faker::Internet.user_name, birthday: Faker::Date.between(60.years.ago, 18.years.ago), phone_number: Faker::PhoneNumber.cell_phone)
+  User.create!(first_name: Faker::Name.first_name, password: 'hellohello', last_name: Faker::Name.last_name, email: Faker::Internet.safe_email, username: Faker::Internet.user_name, birthday: Faker::Date.between(60.years.ago, 18.years.ago), phone_number: Faker::PhoneNumber.cell_phone)
 end
 
 5.times do |index|
