ff4381dfbe8f073d0625fce471e5aaa9190a6015
Ricardo Fleury <rfleury2@gmail.com>
2015-03-25 19:12:19 -0500

    Add game model spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Game, type: :model do
   end
 
   describe 'validations' do
-
+    it { should validate_presence_of(:game_time) }
+    it { should validate_presence_of(:location) }
   end
 end
