fe3061118cd5439bc3a0ccaa12ab697d97f3cc10
Ricardo Fleury <rfleury2@gmail.com>
2015-03-25 19:39:30 -0500

    Add validations and specs for location model

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
+  describe 'associations' do
+    it { should have_many :games }
+    it { should have_many :teams }
+  end
 
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+  end
 end
