87a809ea57c44cce55df6fcf3b4f862b5de17821
Ricardo Fleury <rfleury2@gmail.com>
2015-03-25 19:03:06 -0500

    Incorporate shoulda matchers

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,5 +1,5 @@
 class Rsvp < ActiveRecord::Base
   belongs_to  :membership
-  belongs_to  :player, through: :membership, source: :user
+  # belongs_to  :player, through: :membership, source: :user
   belongs_to  :game
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    # it { should belong_to :team }
+  end
+
+  describe 'validations' do
+
+  end
 end
