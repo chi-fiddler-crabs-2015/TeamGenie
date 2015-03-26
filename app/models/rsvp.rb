80ffc18e6d945bd82ae8fca7c9e6943044313253
Ricardo Fleury <rfleury2@gmail.com>
2015-03-25 19:22:31 -0500

    Add validations and specs for rsvp model

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -2,4 +2,6 @@ class Rsvp < ActiveRecord::Base
   belongs_to  :membership
   # belongs_to  :player, through: :membership, source: :user
   belongs_to  :game
+
+  validates :attending, presence: true
 end
--- spec/models/rsvp_spec.rb
+++ spec/models/rsvp_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Rsvp, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to :membership }
+    it { should belong_to :game }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:attending) }
+
+    it 'should have "pending" assigned to attending' do
+      testRsvp = Rsvp.create!()
+      expect(testRsvp.attending).to eq 'pending'
+    end
+
+  end
 end
