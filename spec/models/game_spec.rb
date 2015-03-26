bede736adb0c925116e30c692b0d2df26f2ac71b
Ricardo Fleury <rfleury2@gmail.com>
2015-03-25 19:09:35 -0500

    Fix shoulda matchers - working

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
   describe 'associations' do
-    # it { should belong_to :team }
+    it { should have_many :rsvps }
+    it { should belong_to :team }
+    it { should belong_to :location }
   end
 
   describe 'validations' do
