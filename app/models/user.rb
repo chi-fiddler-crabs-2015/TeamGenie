86e2e4045978e8547e96fc12b577895975131c6a
David <david.vaynshteyn@gmail.com>
2015-03-25 23:43:11 -0500

    fixed merge conflict in users model from travis build

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ActiveRecord::Base
   has_many  :teams, foreign_key: :captain_id
   has_many  :memberships, foreign_key: :player_id
-<<<<<<< HEAD
-  has_many  :rsvps, through: :memberships #please revisit tmrw
-  has_many  :games, through: :rsvps  #please revisit tmrw
-=======
   has_many  :rsvps, through: :memberships
   has_many  :games, through: :rsvps
 
@@ -20,5 +16,4 @@ class User < ActiveRecord::Base
     uniqueness: true
   }
 
->>>>>>> 5cfee525a9bf8ea938d3f20d325f4f2ce365d044
 end
