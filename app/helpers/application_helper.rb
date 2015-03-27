2ee4b3f956a7ff0218894b9576a2aad08f36c94b
Ricardo Fleury <rfleury2@gmail.com>
2015-03-26 19:56:13 -0500

    Add us_states helper for location new form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
   end
 
+  def us_states
+    ['AL', 'AK',  'AZ', 'AR',  'CA', 'CO',  'CT', 'DE',  'DC', 'FL',  'GA', 'HI',  'ID', 'IL',  'IN', 'IA',  'KS', 'KY',  'LA', 'ME',  'MD', 'MA',  'MI', 'MN',  'MS', 'MO',  'MT', 'NE',  'NV', 'NH',  'NJ', 'NM',  'NY', 'NC',  'ND', 'OH',  'OK', 'OR',  'PA', 'PR',  'RI', 'SC',  'SD', 'TN',  'TX', 'UT',  'VT', 'VA',  'WA', 'WV',  'WI',
+     'WY']
+  end
+
 end
