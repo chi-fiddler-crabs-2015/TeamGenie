4ebcdc8ec3be8e0726ff1ac15a0d544488ae7f7c
David Vaynshteyn & Ricardo Fleury <pair+dvayns2.rfleury2@devbootcamp.com>
2015-03-28 18:31:51 -0500

    Change seeds to assign captain to seed team.  Rescoped jquery to exclude the player rsvp area

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@ Location.create!(name: 'River Park Field', address: '5100 N Francisco', city: 'C
 Location.create!(name: 'Toyota Park', address: '7000 Harlem Ave', city: 'Bridgeview', state: 'IL', zip_code: '60455', directions: "Off of Harlem Ave by Argo Community High School")
 
 team1 = captain.teams.create!(name: 'Good Times FC', activity: 'Soccer', home_location: Location.all.sample)
+captain.memberships.create(team: team1)
 
 20.times do
   team1.players.create!(first_name: Faker::Name.first_name, password: 'hellohello', last_name: Faker::Name.last_name, email: Faker::Internet.safe_email, username: Faker::Internet.user_name, birthday: Faker::Date.between(60.years.ago, 18.years.ago), phone_number: Faker::PhoneNumber.cell_phone)
