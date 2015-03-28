898e013d1d67de0b55342414f4e950c93ac2cd3c
kaylindenberg <kaylindenberg@users.noreply.github.com>
2015-03-28 10:38:12 -0500

    mailing working!

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'kay.lindenberg@gmail.com'
 
   def welcome_email(user)
-    @user = User.first
+    @user = user
     @url  = 'http://teamgenie.herokuapp.com/login'
     mail(to: @user.email, subject: 'Welcome to TeamGenie')
   end
