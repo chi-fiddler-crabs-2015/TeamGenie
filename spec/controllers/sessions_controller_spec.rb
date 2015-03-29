6a4287eb96de169a36e86c0b63154fdb55539fc4
David <david.vaynshteyn@gmail.com>
2015-03-28 23:36:33 -0500

    added test for sessions controller get new

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  let(:user) { create(:user) }
+
+  context "GET #show" do
+    describe 'when valid parameters are passed' do
+      it "should render the login template" do
+        expect(get :new).to render_template("new")
+      end
+    end
+  end
 
 end
