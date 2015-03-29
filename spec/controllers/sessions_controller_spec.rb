3e3ec6ac81de5536e352ba2ffd27f1c0885fa7af
David <david.vaynshteyn@gmail.com>
2015-03-28 23:43:12 -0500

    added sessions controller test for post create

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,10 +3,20 @@ require 'rails_helper'
 RSpec.describe SessionsController, type: :controller do
   let(:user) { create(:user) }
 
-  context "GET #show" do
+  context "GET #new" do
     describe 'when valid parameters are passed' do
       it "should render the login template" do
         expect(get :new).to render_template("new")
+      end
+    end
+  end
+
+  context "POST #create" do
+    describe "when valid parameters are passed" do
+      it "should assign @user to the user logging in" do
+        view =
+          post :create, { user: {email: user.email, password: user.password } }
+        expect(view).to redirect_to root_path
       end
     end
   end
