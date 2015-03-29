a03d1c3408a9bd3101c3ce81bed06e5248cb7053
David Vaynshteyn & Ricardo Fleury <pair+dvayns2.rfleury2@devbootcamp.com>
2015-03-29 10:41:03 -0500

    Updating RSVP status works but no response is given and no auto refresh

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,7 +1,11 @@
 class RsvpsController < ApplicationController
 
-  def update
+  skip_before_action :verify_authenticity_token
 
+  def update
+    rsvp = Rsvp.find_by(id: params[:id])
+    rsvp.attending = params[:attendance]
+    rsvp.save
   end
 
 end
