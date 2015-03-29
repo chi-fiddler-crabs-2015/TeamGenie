977ccb8df8f2f1263111837447ce6b16742232d7
David <david.vaynshteyn@gmail.com>
2015-03-28 22:50:32 -0500

    refactored teams controller new and index test

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe TeamsController, type: :controller do
   let(:user) { create(:user) }
+  let(:location) { Location.create!(name: "here", address: "351 west hubbard", city: "chicago", state: "il", zip_code: "60604", directions: "turn right and left") }
+  let(:team) { user.teams.create!(name: "DBC", activity: "soccer",  home_location: location) }
 
   before(:each) do
     allow_any_instance_of(TeamsController).to receive(:current_user).and_return(user)
@@ -9,8 +11,6 @@ RSpec.describe TeamsController, type: :controller do
   context "GET #index" do
     describe 'when valid parameters are passed' do
       it 'should assign @teams to all teams' do
-        location = Location.create!(name: "here", address: "351 west hubbard", city: "chicago", state: "il", zip_code: "60604", directions: "turn right and left")
-        team = user.teams.create!(name: "DBC", activity: "soccer",  home_location: location)
         get :index
         expect(assigns(:teams)).to eq(Team.all)
       end
@@ -20,8 +20,6 @@ RSpec.describe TeamsController, type: :controller do
   context "GET #new" do
     describe 'when valid parameters are passed' do
       it 'should render the new template' do
-        location = Location.create!(name: "here", address: "351 west hubbard", city: "chicago", state: "il", zip_code: "60604", directions: "turn right and left")
-        team = user.teams.create!(name: "DBC", activity: "soccer",  home_location: location)
         expect(get :new).to redirect_to(new_team)
       end
     end
