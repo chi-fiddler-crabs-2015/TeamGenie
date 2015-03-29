d7362741efa7edd49c6093a5321d61002a7f5e14
David <david.vaynshteyn@gmail.com>
2015-03-29 00:00:57 -0500

    refactored games controller to add logic of location factory

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
   let!(:user) { create(:user) }
+  let!(:location) { create(:location) }
 
   before(:each) do
     allow_any_instance_of(GamesController).to receive(:current_user).and_return(user)
@@ -9,7 +10,6 @@ RSpec.describe GamesController, type: :controller do
 
   context "GET #index" do
     it 'assigns @games to all games' do
-      location = Location.create!(name: "here", address: "351 west hubbard", city: "chicago", state: "il", zip_code: "60604", directions: "turn right and left")
       team = user.teams.create!(name: "DBC", activity: "soccer",  home_location: location)
       game = team.games.create!(game_time: DateTime.now, location: location)
       get :index, :team_id => team.id
@@ -19,7 +19,6 @@ RSpec.describe GamesController, type: :controller do
 
   context "GET #new" do
     it 'instantiates a new game' do
-      location = Location.create!(name: "here", address: "351 west hubbard", city: "chicago", state: "il", zip_code: "60604", directions: "turn right and left")
       team = user.teams.create!(name: "DBC", activity: "soccer",  home_location: location)
       game = team.games.create!(game_time: DateTime.now, location: location)
       get :new, :team_id => team.id
@@ -31,7 +30,6 @@ RSpec.describe GamesController, type: :controller do
     # TEST WITH CAPYBARA
     # describe "when valid parameters are passed" do
     #   it "creates a new game" do
-    #     location = Location.create!(name: "here", address: "351 west hubbard", city: "chicago", state: "il", zip_code: "60604", directions: "turn right and left")
     #     team = user.teams.create!(name: "DBC", activity: "soccer",  home_location: location)
     #     game = team.games.new(game_time: DateTime.now, location: location)
     #     expect {
@@ -43,7 +41,6 @@ RSpec.describe GamesController, type: :controller do
   context "POST #destroy" do
     describe "when valid parameters are passed" do
       it "should delete a game" do
-        location = Location.create!(name: "here", address: "351 west hubbard", city: "chicago", state: "il", zip_code: "60604", directions: "turn right and left")
         team = user.teams.create!(name: "DBC", activity: "soccer",  home_location: location)
         game = team.games.create!(game_time: DateTime.now, location: location)
         expect {
@@ -56,7 +53,6 @@ RSpec.describe GamesController, type: :controller do
     #TEST ON CAPYBARA
     # describe "when valid parameters are passed" do
     #   it "should redirect to the edit team game path" do
-    #     location = Location.create!(name: "here", address: "351 west hubbard", city: "chicago", state: "il", zip_code: "60604", directions: "turn right and left")
     #     team = user.teams.create!(name: "DBC", activity: "soccer",  home_location: location)
     #     game = team.games.create!(game_time: DateTime.now, location: location)
     #     expect(get :edit, { id: game.id, team_id: team.id}).to redirect_to edit_team_game_path(id: game.id, team_id: team.id)
@@ -69,7 +65,6 @@ RSpec.describe GamesController, type: :controller do
     #TEST ON CAPYBARA
     # describe "when valid parameters are passed" do
     #   it "should redirect to the edit team game path" do
-    #     location = Location.create!(name: "here", address: "351 west hubbard", city: "chicago", state: "il", zip_code: "60604", directions: "turn right and left")
     #     team = user.teams.create!(name: "DBC", activity: "soccer",  home_location: location)
     #     game = team.games.create!(game_time: DateTime.now, location: location)
     #     get :show, id: game.to_param, team_id: team.to_param
