2986f3e3641c0f931559b4850657e98aa19a38e1
David Vaynshteyn & Ricardo Fleury <pair+dvayns2.rfleury2@devbootcamp.com>
2015-03-29 11:46:16 -0500

    RSVP MVP!

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -6,6 +6,11 @@ class RsvpsController < ApplicationController
     rsvp = Rsvp.find_by(id: params[:id])
     rsvp.attending = params[:attendance]
     rsvp.save
+    respond_to do |format|
+      format.js do
+        render partial: 'games/show', locals: {player: current_user, game: current_game(params[:game_id])} and return
+      end
+    end
   end
 
 end
