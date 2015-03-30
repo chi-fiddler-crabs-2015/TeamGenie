9acf1ee1725f63207cd7916cf83aff74f225a005
David Vaynshteyn & Ricardo Fleury <pair+dvayns2.rfleury2@devbootcamp.com>
2015-03-30 10:35:49 -0500

    Create new location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,25 @@
 class LocationsController < ApplicationController
 
+  def new
+  end
+
   def show
     @location = find_location(params[:id])
+  end
+
+  def create
+    new_location = Location.create(location_params)
+    if new_location.valid?
+      redirect_to teams_path
+    else
+      render 'new' and return
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :address, :city, :state, :description)
   end
 
 end
