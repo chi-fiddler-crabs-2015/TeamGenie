a1813fba73c86bb5d52817be0e385d746cc38bb3
Ricardo Fleury <rfleury2@gmail.com>
2015-03-30 18:19:08 -0500

    create email_all controller"

--- app/controllers/email_all_controller.rb
+++ app/controllers/email_all_controller.rb
@@ -2,7 +2,10 @@ class EmailAllController < ApplicationController
 
   def create
     @team = find_team(params[:team_id])
-
+    @message = params[:email_all][:message]
+    @team.players.each do |player|
+      EmailAllMailer.delay.email_all(@message, player)
+    end
     redirect_to team_path(@team)
   end
 
