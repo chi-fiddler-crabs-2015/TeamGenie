f1740d6cc8aefe4b10fdeab533b36722c8e603f1
Ricardo Fleury <rfleury2@gmail.com>
2015-03-30 13:17:07 -0500

    Revert seed to include default image

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ Location.create!(name: 'River Park Field', address: '5100 N Francisco', city: 'C
 Location.create!(name: 'Toyota Park', address: '7000 Harlem Ave', city: 'Bridgeview', state: 'IL', zip_code: '60455', directions: "Off of Harlem Ave by Argo Community High School")
 
 # Test the photo on the seeds.
-team1 = captain.teams.create!(name: 'Good Times FC', activity: 'Soccer', home_location: Location.all.sample)
+team1 = captain.teams.create!(name: 'Good Times FC', activity: 'Soccer', home_location: Location.all.sample, team_logo: File.new('public/system/teams/team_logos/default_team_logo/original/default_team_logo.jpg'))
 captain.memberships.create(team: team1)
 User.create!(first_name: 'test1', last_name: 'test1', username: 'test1', email: 't1@t.com', password: 'hellohello')
 User.create!(first_name: 'test2', last_name: 'test2', username: 'test2', email: 't2@t.com', password: 'hellohello')
