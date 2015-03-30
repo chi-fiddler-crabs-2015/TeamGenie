16f33371029574ab2db568c86b4844766d988987
David Vaynshteyn & Ricardo Fleury <pair+dvayns2.rfleury2@devbootcamp.com>
2015-03-30 10:48:33 -0500

    Add rspec to location controller create function

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe LocationsController, type: :controller do
     end
   end
 
+  describe 'CREATE' do
+    it 'does not create a new location for invalid location' do
+      expect{post :create, location: { name: 'hello', state: 'IL', city: 'Chicago' } }.to_not change{Location.count}
+    end
+
+    it 're-renders the new page for invalid location' do
+      expect(post :create, location: { name: 'hello', state: 'IL', city: 'Chicago' } ).to render_template 'new'
+    end
+
+    it 'creates a new location for a valid location' do
+      expect{post :create, location: { name: 'hello', address: '111 State St', state: 'IL', city: 'Chicago' } }.to change{Location.count}.by 1
+    end
+  end
+
 end
