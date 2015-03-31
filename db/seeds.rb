10631c519c6b8ca97be728d65264043e9308d910
David <david.vaynshteyn@gmail.com>
2015-03-30 22:15:09 -0500

    twilio functional for all team text

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ User.create!(first_name: 'test3', last_name: 'test3', username: 'test3', email: 
 
 
 15.times do
-  team1.players.create!(first_name: Faker::Name.first_name, password: 'hellohello', last_name: Faker::Name.last_name, email: Faker::Internet.safe_email, username: Faker::Internet.user_name, birthday: Faker::Date.between(60.years.ago, 18.years.ago), phone_number: Faker::PhoneNumber.cell_phone)
+  team1.players.create!(first_name: Faker::Name.first_name, password: 'hellohello', last_name: Faker::Name.last_name, email: Faker::Internet.safe_email, username: Faker::Internet.user_name, birthday: Faker::Date.between(60.years.ago, 18.years.ago), phone_number: Faker::PhoneNumber.cell_phone.to_s)
 end
 
 10.times do |index|
