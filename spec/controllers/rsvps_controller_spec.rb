24a71e098c86c7d6dbc87594424dcbc8b24c64a6
David <david.vaynshteyn@gmail.com>
2015-03-31 23:00:54 -0500

    changes to the rsvp test

--- spec/controllers/rsvps_controller_spec.rb
+++ spec/controllers/rsvps_controller_spec.rb
@@ -3,6 +3,8 @@ RSpec.describe RsvpsController, type: :controller do
   let!(:location) { create(:location) }
   let!(:team) { user.teams.create!(name: "DBC", activity: "soccer",  home_location: location) }
   let!(:membership) {user.memberships.create!(team: team)}
+  let!(:game) { team.games.create(game_time: DateTime.now, location: location) }
+  let!(:rsvp) { membership.rsvps.create!(game: game) }
 
   before(:each) do
     allow_any_instance_of(MembershipsController).to receive(:current_user).and_return(user)
@@ -14,7 +16,7 @@ RSpec.describe RsvpsController, type: :controller do
       # expect{
       #   put :mark_paid, { membership_id: membership.id, team_id: team.id } }.to change{membership.paid}.from(false).to(true)
       expect(
-        put :mark_paid, { membership_id: membership.id, team_id: team.id } ).to redirect_to team_roster_path(membership.team)
+        put :update, { team_id: team.id, game_id: game.id, id: rsvp.id } ).to render_template('games/show')
     end
   end
 
