f6757341136d4acab342e4d0d1165883be3a9efe
Ricardo Fleury <rfleury2@gmail.com>
2015-04-01 18:47:26 -0500

    Change redirect on user sign up route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
       redirect_to teams_path
     else
       @errors = @user.errors.full_messages
-      render 'new'
+      redirect_to 'new'
     end
   end
 
