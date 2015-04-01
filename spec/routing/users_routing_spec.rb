33f107c7dac8a5ff67c6b17e01a059544a06f533
David Vaynshteyn <david.vaynshteyn@gmail.com>
2015-04-01 11:15:31 -0500

    changed specs for multiple routes for users

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -3,23 +3,23 @@ require "rails_helper"
 RSpec.describe UsersController, :type => :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/teams").to route_to("teams#index")
+      expect(:get => "/users").to route_to("users#index")
     end
 
     it "routes to #new" do
-      expect(:get => "/teams/new").to route_to("teams#new")
+      expect(:get => "/users/new").to route_to("users#new")
     end
 
     it "routes to #create" do
-      expect(:post => "/teams").to route_to("teams#create")
+      expect(:post => "/users").to route_to("users#create")
     end
 
     it "routes to #edit" do
-      expect(:get => "/teams/1/edit").to route_to("teams#edit", :id => "1")
+      expect(:get => "/users/1/edit").to route_to("users#edit", :id => "1")
     end
 
     it "routes to #show" do
-      expect(:get => "/teams/1").to route_to("teams#show", :id => "1")
+      expect(:get => "/users/1").to route_to("users#show", :id => "1")
     end
 
     it "routes to #update" do
