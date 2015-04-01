81b6797b2ad9f9c47dacb55912ce1dcd0e667bf8
David <david.vaynshteyn@gmail.com>
2015-03-31 22:56:45 -0500

    added file for rsvp controller

---
+++ spec/controllers/rsvps_controller_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe RsvpsController, type: :controller do
+  let!(:user) { create(:user) }
+  let!(:location) { create(:location) }
+  let!(:team) { user.teams.create!(name: "DBC", activity: "soccer",  home_location: location) }
+  let!(:membership) {user.memberships.create!(team: team)}
+
+  before(:each) do
+    allow_any_instance_of(MembershipsController).to receive(:current_user).and_return(user)
+  end
+
+  context "UPDATE rsvps#update" do
+    it 'to change rsvp from pending to attending' do
+      expect(membership.paid).to be false
+      # expect{
+      #   put :mark_paid, { membership_id: membership.id, team_id: team.id } }.to change{membership.paid}.from(false).to(true)
+      expect(
+        put :mark_paid, { membership_id: membership.id, team_id: team.id } ).to redirect_to team_roster_path(membership.team)
+    end
+  end
+
+end
