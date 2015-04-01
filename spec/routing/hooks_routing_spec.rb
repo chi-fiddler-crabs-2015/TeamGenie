a079ade2e5dddc1876090050630a1c6c8976b158
David Vaynshteyn <david.vaynshteyn@gmail.com>
2015-04-01 11:29:41 -0500

    made hooks routing spec

--- spec/routing/hooks_routing_spec.rb
+++ spec/routing/hooks_routing_spec.rb
@@ -2,24 +2,8 @@ require "rails_helper"
 
 RSpec.describe StripeController, :type => :routing do
   describe "routing" do
-    it "routes to #oauth" do
-      expect(:get => "/connect/oauth").to route_to("stripe#oauth")
-    end
-
-    it "routes to #confirm" do
-      expect(:get => "/connect/confirm").to route_to("stripe#confirm")
-    end
-
-    it "routes to #deauthorize" do
-      expect(:get => "/connect/deauthorize").to route_to("stripe#deauthorize")
-    end
-
-    it "routes to #managed" do
-      expect(:post => "/connect/managed").to route_to("stripe#managed")
-    end
-
-    it "routes to #standalone" do
-      expect(:post => "/connect/standalone").to route_to("stripe#standalone")
+    it "routes to #stripe" do
+      expect(:post => "/hooks/stripe").to route_to("hooks#stripe")
     end
   end
 end
