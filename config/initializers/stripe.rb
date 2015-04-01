1b4b89bb8a5e6e2c456843343d9a8a039cc511fc
David Vaynshteyn <david.vaynshteyn@gmail.com>
2015-04-01 17:16:56 -0500

    adding to development after changing stripe secret keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,7 @@
+Rails.configuration.stripe = {
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_SECRET_KEY']
+}
+
 Stripe.api_key = Rails.application.secrets.stripe_secret_key
 Stripe.api_version = '2015-02-18'
