f219284c1707a7ee56689409c7c980bb3e3f2f7d
David Vaynshteyn <david.vaynshteyn@gmail.com>
2015-04-01 10:52:24 -0500

    add tests for routing of membership destroy

--- spec/routing/memberships_routing_spec.rb
+++ spec/routing/memberships_routing_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe MembershipsController, :type => :routing do
     it "routes to #mark_paid" do
       expect(:put => "/teams/1/memberships/1/mark_paid").to route_to("memberships#mark_paid", :team_id => "1", :membership_id => "1")
     end
+
+    it "routes to #mark_unpaid" do
+      expect(:put => "/teams/1/memberships/1/mark_unpaid").to route_to("memberships#mark_unpaid", :team_id => "1", :membership_id => "1")
+    end
+
+    it "routes to #destroy" do
+      expect(:delete => "/teams/1/memberships/1").to route_to("memberships#destroy", :team_id => "1", :id => "1")
+    end
   end
 end
