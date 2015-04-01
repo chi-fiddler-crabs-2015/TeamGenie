6894aabc293740044be40d5527a2a75d241cdb87
David <david.vaynshteyn@gmail.com>
2015-03-31 23:19:22 -0500

    need to ask why this test is not working

--- spec/controllers/rsvps_controller_spec.rb
+++ spec/controllers/rsvps_controller_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe RsvpsController, type: :controller do
     it 'to change rsvp from pending to attending' do
       expect(rsvp.attending).to eq "pending"
       expect{
-        put :update, { team_id: team.id, game_id: game.id, id: rsvp.id, attendance: "attending", :format => 'js' } }.to change{ rsvp.attending }.from("pending").to("attending")
+        put :update, { team_id: team.id, game_id: game.id, id: rsvp.id, attendance: "attending", :format => 'js' } }.to change(rsvp, :attending ).from("pending").to("attending")
       expect(
         put :update, { team_id: team.id, game_id: game.id, id: rsvp.id , :format => 'js' } ).to render_template("games/_show")
     end
