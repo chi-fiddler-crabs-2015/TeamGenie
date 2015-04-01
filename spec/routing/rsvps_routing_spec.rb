cc343c21ac5688e0b21c5d5509a806cf15e132d8
David Vaynshteyn <david.vaynshteyn@gmail.com>
2015-04-01 10:59:39 -0500

    fixed rsvp update route test

--- spec/routing/rsvps_routing_spec.rb
+++ spec/routing/rsvps_routing_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe RsvpsController, :type => :routing do
   describe "routing" do
-    it "routes to #destroy" do
-      expect(:delete => "/teams/1/memberships/1").to route_to("memberships#destroy", :team_id => "1", :id => "1")
+    it "routes to #update" do
+      expect(:put => "/teams/1/games/1/rsvps/1").to route_to("rsvps#update", :team_id => "1", :game_id => "1", :id => "1")
     end
   end
 end
