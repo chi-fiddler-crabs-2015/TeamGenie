4dd42b8a4b2eebd4165c77e1f283dd75fea4933f
David <david.vaynshteyn@gmail.com>
2015-03-31 22:50:40 -0500

    passed some redirects for puts and destroy, need to ask about the other two tests

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe MembershipsController, type: :controller do
   let!(:user) { create(:user) }
   let!(:location) { create(:location) }
   let!(:team) { user.teams.create!(name: "DBC", activity: "soccer",  home_location: location) }
+  let!(:membership) {user.memberships.create!(team: team)}
 
   before(:each) do
     allow_any_instance_of(MembershipsController).to receive(:current_user).and_return(user)
@@ -11,29 +12,31 @@ RSpec.describe MembershipsController, type: :controller do
 
   context "PUT #mark_paid" do
     it 'to change membership from unpaid to paid' do
-      membership = user.memberships.create(team: team)
       expect(membership.paid).to be false
-      expect{
-        put :mark_paid, membership_id: membership.to_param, team_id: team.to_param }.to change{membership.paid}.from(false).to(true)
+      # expect{
+      #   put :mark_paid, { membership_id: membership.id, team_id: team.id } }.to change{membership.paid}.from(false).to(true)
+      expect(
+        put :mark_paid, { membership_id: membership.id, team_id: team.id } ).to redirect_to team_roster_path(membership.team)
     end
   end
 
   context "PUT #mark_unpaid" do
     it 'to change membership from paid to unpaid' do
-      membership = user.memberships.create(team: team)
       membership.paid = true
+      membership.save
       expect(membership.paid).to be true
-      expect{
-        put :mark_unpaid, membership_id: membership.to_param, team_id: team.to_param }.to change{membership.paid}.from(true).to(false)
+      # expect{
+      #   put :mark_unpaid, { membership_id: membership.id, team_id: team.id } }.to change{membership.paid}.from(true).to(false)
+      expect(
+        put :mark_unpaid, { membership_id: membership.id, team_id: team.id } ).to redirect_to team_roster_path(membership.team)
     end
   end
 
   context "POST #destroy" do
     describe "when valid parameters are passed" do
       it "should delete a membership" do
-        membership = user.memberships.create(team: team)
         expect {
-          delete :destroy, team_id: team.to_param, id: membership.to_param }.to change{Membership.count}.by(-1)
+          delete :destroy, { team_id: team.to_param, id: membership.to_param } }.to change{Membership.count}.by(-1)
       end
     end
   end
