002a96b5d28e03efd62d1bae7504faf78e9a3d84
David <david.vaynshteyn@gmail.com>
2015-03-31 22:08:12 -0500

    added test for destroying a membership

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -10,13 +10,32 @@ RSpec.describe MembershipsController, type: :controller do
   end
 
   context "PUT #mark_paid" do
-    it 'assigns @games to all games' do
+    it 'to change membership from unpaid to paid' do
       membership = user.memberships.create(team: team)
       expect(membership.paid).to be false
-      put :mark_paid, :membership_id => membership.id, :team_id => team.id
-      expect(membership.paid).to be true
+      expect{
+        put :mark_paid, membership_id: membership.to_param, team_id: team.to_param }.to change{membership.paid}.from(false).to(true)
     end
   end
 
+  context "PUT #mark_unpaid" do
+    it 'to change membership from paid to unpaid' do
+      membership = user.memberships.create(team: team)
+      membership.paid = true
+      expect(membership.paid).to be true
+      expect{
+        put :mark_unpaid, membership_id: membership.to_param, team_id: team.to_param }.to change{membership.paid}.from(true).to(false)
+    end
+  end
+
+  context "POST #destroy" do
+    describe "when valid parameters are passed" do
+      it "should delete a membership" do
+        membership = user.memberships.create(team: team)
+        expect {
+          delete :destroy, team_id: team.to_param, id: membership.to_param }.to change{Membership.count}.by(-1)
+      end
+    end
+  end
 
 end
