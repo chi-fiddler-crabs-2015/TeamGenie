ff09e503923b34268e8b050556dbd190a43b8fa0
David <david.vaynshteyn@gmail.com>
2015-03-31 21:40:26 -0500

    test is running need to fix

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe MembershipsController, type: :controller do
   let!(:user) { create(:user) }
   let!(:location) { create(:location) }
+  let!(:team) { user.teams.create!(name: "DBC", activity: "soccer",  home_location: location) }
 
   before(:each) do
     allow_any_instance_of(MembershipsController).to receive(:current_user).and_return(user)
+  end
+
+  context "PUT #mark_paid" do
+    it 'assigns @games to all games' do
+      membership = user.memberships.create(team: team)
+      expect(membership.paid).to be false
+      put :mark_paid, :membership_id => membership.id, :team_id => team.id
+      expect(membership.paid).to be true
+    end
   end
 
 
