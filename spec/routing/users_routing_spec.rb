d6bdde8bdc9f28c3fb18c2e21528567648b86db2
David Vaynshteyn <david.vaynshteyn@gmail.com>
2015-04-01 11:13:54 -0500

    made file for users routing controller

---
+++ spec/routing/users_routing_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.describe UsersController, :type => :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(:get => "/teams").to route_to("teams#index")
+    end
+
+    it "routes to #new" do
+      expect(:get => "/teams/new").to route_to("teams#new")
+    end
+
+    it "routes to #create" do
+      expect(:post => "/teams").to route_to("teams#create")
+    end
+
+    it "routes to #edit" do
+      expect(:get => "/teams/1/edit").to route_to("teams#edit", :id => "1")
+    end
+
+    it "routes to #show" do
+      expect(:get => "/teams/1").to route_to("teams#show", :id => "1")
+    end
+
+    it "routes to #update" do
+      expect(:put => "/teams/1").to route_to("teams#update", :id => "1")
+    end
+
+    it "routes to #destroy" do
+      expect(:delete => "/teams/1").to route_to("teams#destroy", :id => "1")
+    end
+
+    it "routes to #roster" do
+      expect(:get => "/teams/1/roster").to route_to("teams#roster", :team_id => "1")
+    end
+
+    it "routes to ##distribute_dues" do
+      expect(:put => "/teams/1/distribute_dues").to route_to("teams#distribute_dues", :team_id => "1")
+    end
+  end
+end
