be8abd5cc1781c86beb30984d02f9b9cdc2c6b8d
David <david.vaynshteyn@gmail.com>
2015-04-01 20:40:58 -0500

    added scenarios for home page and navigating to login page

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,1 +1,23 @@
 require 'rails_helper'
+
+feature "User navigating to the home page" do
+
+  scenario "when user visits homepage user can navigate to login/signup" do
+    visit root_path
+    expect(page).to have_content("LOG IN")
+    expect(page).to have_content("SIGN UP")
+  end
+
+  scenario "when user clicks on login it will redirect to the login page" do
+    visit root_path
+    click_link("LOG IN")
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password")
+  end
+
+  scenario "when user enters information in the login form they are redirected to my teams" do
+    visit login_path
+    fill_in
+
+  end
+end
