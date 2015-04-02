1ba41d69334d25a19a54897ad8b616c426e2624c
Ricardo Fleury <rfleury2@gmail.com>
2015-04-01 19:12:27 -0500

    File size limit expanded

--- app/models/team.rb
+++ app/models/team.rb
@@ -12,7 +12,7 @@ class Team < ActiveRecord::Base
   validates   :name, presence: true
 
   validates_attachment_content_type :team_logo, :content_type => /\Aimage\/.*\Z/
-  validates_attachment_size :team_logo, less_than: 1.megabyte
+  validates_attachment_size :team_logo, less_than: 10.megabytes
 
   def future_games
     self.games.select do |game|
