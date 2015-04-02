735d9d38cd738af291fa0669d7af9b4e55f2d0f9
David <david.vaynshteyn@gmail.com>
2015-04-01 23:05:03 -0500

    halfway through features testing for teams

--- spec/features/team_spec.rb
+++ spec/features/team_spec.rb
@@ -4,6 +4,7 @@ feature "Team navigation:" do
   let!(:user) {create(:user)}
   let!(:location) { create(:location) }
   let!(:team) { user.teams.create!(name: "DBC", activity: "soccer",  home_location: location) }
+  let!(:team2) { user.teams.create!(name: "Random", activity: "soccer",  home_location: location) }
 
   before(:each) do
     allow_any_instance_of(TeamsController).to receive(:current_user).and_return(user)
@@ -29,10 +30,13 @@ feature "Team navigation:" do
     expect(page).to have_content team.name
   end
 
-  scenario "when user clicks team name they are taken to the team homepage" do
-    visit teams_path
-    click_link(team.name)
-    expect(page).to have_content("Manage Your Team:")
-  end
+  # scenario "when user clicks team name they are taken to the team homepage" do
+  #   visit new_team_path
+  #   fill_in "Name", :with => team2.name
+  #   fill_in "Activity", :with => team2.activity
+  #   click_button("Sign up")
+  #   click_link("Random")
+  #   expect(page).to have_content("Manage Your Team:")
+  # end
 
 end
