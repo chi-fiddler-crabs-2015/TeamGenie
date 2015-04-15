a77989bdf88a2caa529486edf36ea5d6edfdfd1f
kaylindenberg <kay.lindenberg@gmail.com>
2015-04-15 13:13:16 -0500

    fixed mailer issue

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
   default from: 'TeamGenieSports@gmail.com'
 
   def welcome_email(user)
+  	@user = user
     @url  = 'http://teamgenie.herokuapp.com/login'
     mail(to: user.email, subject: 'Welcome to TeamGenie')
   end
