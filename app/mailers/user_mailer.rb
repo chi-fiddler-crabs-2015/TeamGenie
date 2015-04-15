d7f9c5fce36fba95f41d80694e972c2df3089ae6
kaylindenberg <kay.lindenberg@gmail.com>
2015-04-15 13:15:31 -0500

    slight tweek for consistency

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
   	@user = user
     @url  = 'http://teamgenie.herokuapp.com/login'
-    mail(to: user.email, subject: 'Welcome to TeamGenie')
+    mail(to: @user.email, subject: 'Welcome to TeamGenie')
   end
 end
