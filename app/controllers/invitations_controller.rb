45888e61482a972ce96956b459920643798336cb
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2015-04-16 11:53:46 -0500

    Invitation signup complete

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -8,11 +8,7 @@ class InvitationsController < ApplicationController
       InvitationMailer.delay.team_invitation(@invited_user, @team)
       assign_user_to_team(@team, @invited_user)
     else
-      new_user = User.create(email: params[:invitation][:email], first_name: "Waiting For User", last_name: " to Sign Up", username: "placeholder#{rand(9999999999999)}", password: "placeholder", phone_number: '1111111111')
-      if new_user.valid?
-        InvitationMailer.delay.sign_up_invitation(new_user, @team)
-        assign_user_to_team(@team, new_user)
-      end
+      InvitationMailer.delay.sign_up_invitation(new_user, @team)
     end
     render :nothing => true
   end
