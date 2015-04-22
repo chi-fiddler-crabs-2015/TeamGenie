e58ad9475f6a2791654a65e4f0fa415d09fba9a9
Ricardo Fleury <ricardofleury@ricardos-mbp.gateway.pace.com>
2015-04-22 16:27:38 -0500

    invitation emails functioning

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,12 +3,12 @@ class InvitationsController < ApplicationController
   def create
     @invited_user = User.find_by(email: params[:invitation][:email])
     @team = find_team(params[:team_id])
-    team_captain(@team)
     if @invited_user
       InvitationMailer.delay.team_invitation(@invited_user, @team)
       assign_user_to_team(@team, @invited_user)
     else
-      InvitationMailer.delay.sign_up_invitation(new_user, @team)
+      email = params[:invitation][:email]
+      InvitationMailer.delay.sign_up_invitation(email, @team)
     end
     render :nothing => true
   end
